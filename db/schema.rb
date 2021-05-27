62b119189a21bd459193a2a1bb89ecc9474be479
KENTA MITANI <mitani.kenta88@gmail.com>
2021-05-27 17:08:31 +0900

    memosテーブルにcategoryカラムを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_27_080239) do
+ActiveRecord::Schema.define(version: 2021_05_27_080640) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(version: 2021_05_27_080239) do
     t.string "body"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "category_id"
   end
 
 end
