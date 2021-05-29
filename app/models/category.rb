add46c4cc649d5f5efb9806bab89e5c4e8718e13
KENTA MITANI <mitani.kenta88@gmail.com>
2021-05-29 11:13:45 +0900

    memo＆categoryテーブルにバリデーション追加

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,6 @@
 class Category < ApplicationRecord
-  has_many: memos
+  has_many :memos
+  validates :name, presence: true
+  validates :body, presence: true
+  validates :category_id, presence: true
 end
