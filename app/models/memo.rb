bc77f4a6f7d3edc27e73818dbc1b1c4de19544cd
KENTA MITANI <mitani.kenta88@gmail.com>
2021-06-01 00:40:47 +0900

    models/memo.rbにバリデーション追加

--- app/models/memo.rb
+++ app/models/memo.rb
@@ -1,6 +1,6 @@
 class Memo < ApplicationRecord
   belongs_to :category
-  validates :title, presence: true
-  validates :body, presence: true
+  validates :title, presence: true, length: { maximum: 15 }
+  validates :body, presence: true, length: { maximum: 100 }
   validates :category_id, presence: true
 end
