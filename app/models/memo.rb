25ae4b66865d539f4b159ab407bd24fd02855390
KENTA MITANI <mitani.kenta88@gmail.com>
2021-06-01 22:21:48 +0900

    models/memo.rb csvエクスポート関係の不要なコード削除

--- app/models/memo.rb
+++ app/models/memo.rb
@@ -3,13 +3,4 @@ class Memo < ApplicationRecord
   validates :title, presence: true, length: { maximum: 15 }
   validates :body, presence: true, length: { maximum: 100 }
   validates :category_id, presence: true
-
-  def self.generate_csv
-    CSV.generate do |csv|
-      csv << CSV_COLUMNS
-      all.each do |memo|
-        csv << CSV_COLUMNS.map { |col| memo.send(col) }
-      end
-    end
-  end
 end
